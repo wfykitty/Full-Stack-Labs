e235b3c091ebf71cc16a1bd7637f0765592ee291
wfykitty <feiya.wang@georgebrown.ca>
2020-01-18 21:50:15 -0500

    add my notes

---
+++ js.js
@@ -0,0 +1,9 @@
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+  let name = prompt('Enter a new name');
+  para.textContent = 'Player 1: ' + name;
+}
+
