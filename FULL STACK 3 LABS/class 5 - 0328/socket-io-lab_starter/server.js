32c04b028c8fca0d3b2c06dfc207f0ef7f24e127
wfykitty <feiya.wang@georgebrown.ca>
2020-03-28 14:06:17 -0400

    lab on Mar.28

---
+++ FULL STACK 3 LABS/class 5 - 0328/socket-io-lab_starter/server.js
@@ -0,0 +1,63 @@
+var http = require('http')
+  , url = require('url')
+  , fs = require('fs')
+  , io = require('socket.io')
+  , server;
+
+
+(server = http.createServer(function(req, res){
+  
+  var path = url.parse(req.url).pathname;
+  switch (path){
+    case '/':
+      fs.readFile(__dirname + '/index.html', function(err, data){
+          if (err) return send404(res);
+          res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
+          res.write(data, 'utf8');
+          res.end();
+      });
+    break;
+
+    default: send404(res);
+  }
+})),
+
+(send404 = function(res){
+  res.writeHead(404);
+  res.write('404');
+  res.end();
+});
+
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.listen(port, hostname, ()=> {
+  console.log(`Server running at http://${hostname}:${port}/`);
+})
+
+// socket.io, setup
+var io = io.listen(server);
+
+io.on("connection", function(client) {
+  console.log("connection accepted.");
+
+  //event listeners
+client.on("message", function(message) {
+  console.log(`received message: ${message} from client`);
+  client.emit('msgreceived');
+});
+
+client.on("disconnect", function(message) {
+  console.log("disconnected..");
+ 
+});
+
+});
+
+
+// next time, try to index properly
+// makes your life easier when you debug
+// noooooooo
+// do not do that....
