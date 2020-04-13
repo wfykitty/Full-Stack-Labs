cedbc92dc81924a1fed9c7239ba74e98e69b27b8
wfykitty <feiya.wang@georgebrown.ca>
2020-04-13 16:28:53 -0400

    update

--- FULL_STACK_4_LABS/lab4/src/DeleteStudent.js
+++ FULL_STACK_4_LABS/lab4/src/DeleteStudent.js
@@ -9,7 +9,6 @@ class DeleteStudent extends React.Component {
     this.state = {id: this.props.id}
 
     this.handleSubmit = this.handleSubmit.bind(this); 
-    
   }
 
     handleSubmit = event => {event.preventDefault();
